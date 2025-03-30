23b930bf8ba70dd4c2fc3aa48a947b3972d6cb58
Equipo De Desarrollo <desarrollo@softline.com.co>
2025-03-30 12:37:04 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto#1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Piedra, Papel o Tiera");
+
+while (true)
+{
+    Console.WriteLine("Están listos?");
+    Console.WriteLine("Entonces vamos!!!");
+}
+
+string SelectChoice()
+{
+
+    Console.WriteLine("Escoge P, PP o T: [P]iedra, [PP]apel o [T]ijera: ");
+    var selectedChoice = Console.ReadLine();
+
+    if (selectedChoice?.ToLower() != "p" && selectedChoice?.ToLower() != "pp" && selectedChoice?.ToLower() != "t")
+    {
+        Console.WriteLine("Por favor, seleccione una de estas opciones P, PP o T");
+        selectedChoice = SelectChoice();
+    }
+
+    return selectedChoice;
+}
